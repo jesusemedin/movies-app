31e02f227f133362d9802a2eb700ecf8bd336c8b
Jesus Medina <medinabotamail@gmail.com>
2020-11-10 13:22:23 -0400

    first commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Películas en cines'),
+          backgroundColor: Colors.redAccent[700],
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {})
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              _swiperTarjetas()
+            ],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+
+    return CardSwiper(
+      peliculas: [1, 2, 3, 4, 5],
+    );
+
+  }
+
+}
